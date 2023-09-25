50803c1f2111621aaa63fbf42d2eed7b4dc26b50
SuperAuguste <19855629+SuperAuguste@users.noreply.github.com>
2023-09-25 18:07:42 -0400

    symbols perf

--- docker-images/syntax-highlighter/crates/cody-rpc/src/main.rs
+++ docker-images/syntax-highlighter/crates/cody-rpc/src/main.rs
@@ -1,18 +1,57 @@
-use std::{path::Path, time::Instant};
+use std::{
+    collections::HashMap,
+    path::Path,
+    time::{Duration, Instant},
+};
 
 use gix::{bstr::ByteSlice, objs::tree::EntryMode, traverse::tree::Recorder};
 use scip_syntax::{languages::get_tag_configuration, symbols::parse_tree};
 use scip_treesitter_languages::parsers::BundledParser;
+use tabled::{Table, Tabled};
 
 fn main() {
-    let now = Instant::now();
     let n = find_all_lens_git();
-    println!("{}", now.elapsed().as_millis());
-    println!("{}", n);
+
+    let mut entries = vec![];
+
+    for (k, v) in &n {
+        let secs = Duration::from_nanos(v.nanos as u64).as_secs_f64();
+
+        entries.push(TableEntry {
+            language: k.get_language_name().to_string(),
+            symbols: v.symbols,
+            lines: v.lines,
+            symbols_per_second: ((v.symbols as f64) / (secs)) as usize,
+            lines_per_second: ((v.lines as f64) / (secs)) as usize,
+        });
+    }
+
+    println!("{}", Table::new(entries).to_string());
 }
 
-fn find_all_lens_git() -> usize {
-    let mut n = 0;
+#[derive(Tabled)]
+struct TableEntry {
+    language: String,
+    symbols: usize,
+    lines: usize,
+
+    symbols_per_second: usize,
+    lines_per_second: usize,
+}
+
+#[derive(Debug)]
+struct LangStats {
+    symbols: usize,
+    lines: usize,
+    nanos: u128,
+}
+
+type StatsMap = HashMap<BundledParser, LangStats>;
+
+fn find_all_lens_git() -> StatsMap {
+    // let mut n = 0;
+    let mut map = StatsMap::new();
+
     let repo = gix::open("/Users/auguste.rame/Documents/Repos/sourcegraph/.git").expect("bruh");
 
     let tree = repo
@@ -30,6 +69,7 @@ fn find_all_lens_git() -> usize {
     for a in recorder.records {
         if a.mode == EntryMode::Blob {
             // if a.filepath.ends_with(".ts".as_bytes()) {
+            let now = Instant::now();
             let bundled_parser = if let Some(parser) =
                 BundledParser::get_parser_from_extension(if let Ok(path) = a.filepath.to_str() {
                     Path::new(path)
@@ -69,10 +109,19 @@ fn find_all_lens_git() -> usize {
             )
             .expect("a");
 
-            n += scope.into_document(hint, vec![]).occurrences.len()
+            let nanos = now.elapsed().as_nanos();
+            // n +=
+            let entry = map.entry(bundled_parser).or_insert_with(|| LangStats {
+                symbols: 0,
+                lines: 0,
+                nanos: 0,
+            });
+            entry.lines += source.lines().count();
+            entry.symbols += scope.into_document(hint, vec![]).occurrences.len();
+            entry.nanos += nanos;
             // println!("{:#?}", );
             // }
         }
     }
-    return n;
+    return map;
 }
